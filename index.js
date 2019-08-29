dd7ea14f5f46f47b7853a57792fa48ee946501e9
zau19 <54337274+zau19@users.noreply.github.com>
2019-08-28 21:08:50 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// function isEven(value) {
+//     if (value % 2 == 0)
+//         console.log("true");
+//     else
+//         console.log("false");
+// }
+
+// isEven(4);
+
+
+// n! = n * (n - 1) * (n - 2) * ...*1
+// 4! = 4 * 3 * 2 * 1 * =
+
+// function factorial
+
+//  function factorial(n){
+//      return (n != 1) ? n * factorial(n - 1) : 1;
+//  }
+//  console.log( factorial(4) );
+//  console.log( factorial(6) );
+
+
+// function kebabToSnake(str) {
+//     var myString = str.replace(/-/g, "_");
+//     console.log(myString);
+// }
+// kebabToSnake('hello-world');
+// kebabToSnake('dogs-are-awsome');
